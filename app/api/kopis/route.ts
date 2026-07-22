a4ca0d151e6fc3f57d051e4fc29ce33059b45cf3
남민우 <sscc20111@naver.com>
2026-07-22 22:01:01 +0900

    vercel 배포 테스트

--- app/api/kopis/route.ts
+++ app/api/kopis/route.ts
@@ -1,11 +1,14 @@
 export async function GET() {
+  // const Key = process.env.SEOUL_KEY;
+  const Key = process.env.KPOPS_KEY;
   try {
-    const res = await fetch(process.env.API1_URL!, {
+    const res = await fetch(`http://www.kopis.or.kr/openApi/restful/pblprfr?service=${Key}&stdate=20230601&eddate=20230630&cpage=1&rows=10&prfstate=02&signgucode=11&signgucodesub=1111&kidstate=Y`, {
       // 필요하면 헤더/인증 추가
-      headers: { Authorization: `Bearer ${process.env.API1_KEY}` },
+      // headers: { Authorization: `http://www.kopis.or.kr/openApi/restful/pblprfr?service=${Key}&stdate=20230601&eddate=20230630&cpage=1&rows=10&prfstate=02&signgucode=11&signgucodesub=1111&kidstate=Y` },
       cache: "no-store",
     });
-
+    
+    console.log("Key:", Key);
     const contentType = res.headers.get("content-type") || "";
     const raw = await res.text(); // JSON이든 XML이든 일단 text로 받아버림
 
